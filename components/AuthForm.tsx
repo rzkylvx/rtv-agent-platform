5cdcecda4c89f276a30f3107d90585b40cc9b95b
rzkylvx <rzkylvx@gmail.com>
2025-11-23 18:34:09 +0700

    feat: implement user authentication flow with sign-in and session management; enhance AuthForm and layout components

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -11,9 +11,12 @@ import Link from "next/link";
 import { toast } from "sonner";
 import FormField from "./FormField";
 import { useRouter } from "next/navigation";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "@/firebase/client";
-import { signUp } from "@/lib/actions/auth.action";
+import { signIn, signUp } from "@/lib/actions/auth.action";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -62,6 +65,23 @@ const AuthForm = ({ type }: { type: FormType }) => {
         toast.success("Account created successfully. Please sign in.");
         router.push("/sign-in");
       } else {
+        const { email, password } = values;
+
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const idToken = await userCredential.user.getIdToken();
+
+        if (!idToken) {
+          toast.error("Sign in failed");
+          return;
+        }
+
+        await signIn({ email, idToken });
+
         toast.success("Sign in successfully.");
         router.push("/");
       }
