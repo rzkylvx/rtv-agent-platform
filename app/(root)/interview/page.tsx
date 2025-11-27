0c194748ebe74bf491b74b8fe8a9b4352328686a
rzkylvx <rzkylvx@gmail.com>
2025-11-27 18:50:24 +0700

    feat: enhance Interview and Agent components with user data handling and feedback generation logic

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,14 +1,17 @@
-import Agent from '@/components/Agent'
-import React from 'react'
+import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
+import React, { use } from "react";
 
-const Page = () => {
+const Page = async () => {
+  const user = await getCurrentUser();
+
   return (
     <>
       <h3>Interview Generation</h3>
 
-      <Agent userName="You"/>
+      <Agent userName={user?.name} userId={user?.id} type="generate" />
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
