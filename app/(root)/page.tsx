04ac91fa81630707ae912042f1be1c19d9e2a5f6
rzkylvx <rzkylvx@gmail.com>
2025-11-28 19:06:25 +0700

    feat: implement user interview retrieval and display logic in the Interview section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,17 @@ import Link from "next/link";
 import Image from "next/image";
 import { dummyInterviews } from "@/constants";
 import InterviewCard from "@/components/InterviewCard";
+import {
+  getCurrentUser,
+  getInterviewsByUserId,
+} from "@/lib/actions/auth.action";
 
-const Page = () => {
+const Page = async () => {
+  const user = await getCurrentUser();
+  const userInterviews = await getInterviewsByUserId(user?.id!);
+
+  const hasPastInterviews = userInterviews?.length > 0;
+
   return (
     <>
       <section className="card-cta">
@@ -31,9 +40,13 @@ const Page = () => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
         <div className="interviews-section">
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>You haven&apos;t taken any interviews yet.</p>
+          )}
         </div>
       </section>
 
